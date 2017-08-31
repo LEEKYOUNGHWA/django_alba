f9842d67639102b251bbceef71f43c7a2c1bb273
LEEKYOUNGHWA <rudghk5220@gmail.com>
2017-08-31 18:08:35 +0900

    디자인수정

--- simple-file-upload/uploads/core/static/acidus/HTML/js/layout.js
+++ simple-file-upload/uploads/core/static/acidus/HTML/js/layout.js
@@ -20,11 +20,110 @@ var Layout = function () {
         }
     }
 
+    // Handle Header
+    var handleOnePageHeader = function() {
+        // jQuery to collapse the navbar on scroll
+        if ($('.navbar').offset().top > 150) {
+            $('.navbar-fixed-top').addClass('top-nav-collapse');
+        }
+        $(window).scroll(function() {
+            if ($('.navbar').offset().top > 150) {
+                $('.navbar-fixed-top').addClass('top-nav-collapse');
+            } else {
+                $('.navbar-fixed-top').removeClass('top-nav-collapse');
+            }
+        });
+
+        var $offset = 0;
+        $offset = $(".navbar-fixed-top").height()-20;
+        
+        // jQuery for page scrolling feature - requires jQuery Easing plugin
+        $('.js_nav-item a').bind('click', function(event) {
+            var $position = $($(this).attr('href')).offset().top;
+            $('html, body').stop().animate({
+                scrollTop: $position - $offset
+            }, 600);
+            event.preventDefault();
+        });
+
+        var $scrollspy = $('body').scrollspy({target: '.navbar-fixed-top', offset: $offset+2});
+
+        // Collapse Navbar When It's Clickicked
+        $(window).scroll(function() {
+            $('.navbar-collapse.in').collapse('hide');
+        });
+    }
+
+    // handle carousel
+    var handleCarousel = function() {
+        var $item = $('.carousel .item'); 
+        var $wHeight = $(window).height();
+        $item.eq(0).addClass('active');
+        $item.height($wHeight); 
+        $item.addClass('full-screen');
+
+        $('.carousel img').each(function() {
+            var $src = $(this).attr('src');
+            var $color = $(this).attr('data-color');
+            $(this).parent().css({
+                'background-image' : 'url(' + $src + ')',
+                'background-color' : $color
+            });
+            $(this).remove();
+        });
+
+        $(window).on('resize', function (){
+            $wHeight = $(window).height();
+            $item.height($wHeight);
+        });
+    }
+
+    // handle group element heights
+    var handleHeight = function() {
+       $('[data-auto-height]').each(function() {
+            var parent = $(this);
+            var items = $('[data-height]', parent);
+            var height = 0;
+            var mode = parent.attr('data-mode');
+            var offset = parseInt(parent.attr('data-offset') ? parent.attr('data-offset') : 0);
+
+            items.each(function() {
+                if ($(this).attr('data-height') == "height") {
+                    $(this).css('height', '');
+                } else {
+                    $(this).css('min-height', '');
+                }
+
+                var height_ = (mode == 'base-height' ? $(this).outerHeight() : $(this).outerHeight(true));
+                if (height_ > height) {
+                    height = height_;
+                }
+            });
+
+            height = height + offset;
+
+            items.each(function() {
+                if ($(this).attr('data-height') == "height") {
+                    $(this).css('height', height);
+                } else {
+                    $(this).css('min-height', height);
+                }
+            });
+
+            if(parent.attr('data-related')) {
+                $(parent.attr('data-related')).css('height', parent.height());
+            }
+       });
+    }
+
     return {
         init: function () {
             // initial setup for fixed header
             handleHeaderOnScroll();
-
+            handleOnePageHeader(); // initial header
+            handleCarousel(); // initial setup for carousel
+            handleHeight(); // initial setup for group element height
+            
             // handle minimized header on page scroll
             $(window).scroll(function() {
                 handleHeaderOnScroll();
